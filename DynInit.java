fc34f59053ce412bed6b4475fd24bb9a5f2d210a
StrangerTux <red.stranger.gale@gmail.com>
2023-08-03 01:35:54 +0600

    dynamic type. page 77

---
+++ DynInit.java
@@ -0,0 +1,8 @@
+class Dynlnit {
+	public static void main(String[] args) {
+		double radius = 4, height = 5;
+		// Динамически инициализировать volume ,
+		double volume = Math.PI * radius * radius * height;
+		System.out.println("Объем составляет " + volume);
+	}
+}
