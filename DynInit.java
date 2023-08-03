846b36be19d55b4ee5707ecf6765bb45856c1f99
StrangerTux <red.stranger.gale@gmail.com>
2023-08-03 16:19:07 +0600

    page 80

--- DynInit.java
+++ DynInit.java
@@ -2,7 +2,7 @@ class Dynlnit {
 	public static void main(String[] args) {
 		double radius = 4, height = 5;
 		// Динамически инициализировать volume ,
-		double volume = Math.PI * radius * radius * height;
+		double volume = 3.14 * radius * radius * height;
 		System.out.println("Объем составляет " + volume);
 	}
 }
