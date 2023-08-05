a2e4f40567d174843439649ccf8e5fbfc341078a
StrangerTux <red.stranger.gale@gmail.com>
2023-08-05 20:19:27 +0600

    page 109

--- Guess.java
+++ Guess.java
@@ -4,5 +4,10 @@ class Guess{
 		System.out.print("угадайте букву от A до Z\nВведите букву: ");
 		ch = (char) System.in.read();
 		if (ch == answer) System.out.println("Правильно!");
+		else {
+			System.out.print("Неверно! Буква находится ");
+			if(ch < answer) System.out.println("Ближе к Z");
+			else System.out.println("Ближе к A");
+		}
 	}
 }
