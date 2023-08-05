0e59cce1e441fc03fe1b8a9a96a4e978dc4c6972
StrangerTux <red.stranger.gale@gmail.com>
2023-08-05 18:50:35 +0600

    page 100

---
+++ Guess.java
@@ -0,0 +1,8 @@
+class Guess{
+	public static void main(String[] args) throws java.io.IOException{
+		char ch, answer = 'K';
+		System.out.print("угадайте букву от A до Z\nВведите букву: ");
+		ch = (char) System.in.read();
+		if (ch == answer) System.out.println("Правильно!");
+	}
+}
